204be0bb21507d8dbd8c4187616e7b926d8f1079
Canon Jackson <cmj01059@uga.edu>
2023-09-21 23:48:58 -0400

    First implementation of list, no driver yet

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-## Getting Started
+Merge function
+Make new list object O(1)
+Insert each item of list 1 into list O(n) * O(n)
+Insert each item of list 2 into list O(n) * O(n)
+return list O(1)
+O(n^2)
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Delete alternating
+temp =  head    1
+for(list length) {      n
+    if (count is even)  n
+    delete(temp.info);  n/2 * n
+    temp = temp.next    n
+}
+O(n^2)
 
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Intersection
+list = new list
+temp = head
+while (temp != null) {                  n
+    if (temp.item exists in list)       
+        insert item into list           n*n
+    iterate
+} 
+loop through new list to print          n
+O(n^2)
---
+++ src/SortedLinkedList.java
@@ -0,0 +1,165 @@
+public class SortedLinkedList {
+    
+    private NodeType head;
+    private NodeType currentPos;
+
+    public SortedLinkedList() {
+        head = new NodeType();
+        currentPos = head;
+    }
+
+    public int getLength() {
+        NodeType temp = head;
+        int count = 0;
+
+        while (temp.next != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        return count;
+    }
+
+    public void insertItem(ItemType item) {
+        try {
+            if (item.compareTo(head.info) == -1) {
+                NodeType temp = new NodeType();
+                temp.info = item;
+                temp.next = head;
+                head = temp;
+            }
+        } catch (NullPointerException npe) {
+            head.info = item;
+        }
+        if (head.next == null) {
+            NodeType temp = new NodeType();
+            temp.info = item;
+            head.next = temp;
+        }
+
+        NodeType temp = head;
+        while (temp.next != null) {
+            switch (item.compareTo(temp.next.info)) {
+                case -1:                         
+                    NodeType newNode = new NodeType();
+                    newNode.info = item;
+                    newNode.next = temp.next;
+                    temp.next = newNode;
+                    break;
+
+                case 1:
+                    temp = temp.next;
+                    break;
+
+                default:
+                    System.out.println("Sorry. You cannot insert the duplicate item");
+                    break;
+            }
+        }  
+    }
+
+    public void deleteItem(ItemType item) {
+        try{
+            if (item.compareTo(head.info) == 0) {
+                head = head.next;
+            }
+        } catch (NullPointerException npe) {
+            System.out.println("List is empty");
+        }
+
+        NodeType temp = head;
+        boolean isFound = false;
+        while (!isFound) {
+            try {
+                if (item.compareTo(temp.next.info) == 0) {
+                    temp.next = temp.next.next;
+                    isFound = true;
+                } 
+                else
+                    temp = temp.next;
+            } catch (NullPointerException npe) {
+                System.out.println("The end of the list has been reached");
+                isFound = true;
+            }
+        }
+    }
+    
+    public int searchItem(ItemType item) {
+        int index = 0;
+        NodeType temp = head;
+        while (temp.next != null) {
+            if (item.compareTo(temp.info) == 0) {
+                return index;
+            } else if (item.compareTo(temp.info) == -1) {
+                temp = temp.next;
+                index++;
+            } else {
+                System.out.println("The end of the list had been reached");
+                return -1;
+            }
+        }
+        if (item.compareTo(temp.info) == 0) {
+            return index;
+        } else {
+            System.out.println("The end of the list had been reached");
+            return -1;
+        }
+    }
+
+    public ItemType getNextItem() {
+        ItemType returnItem;
+        if (head.info == null) {
+            System.out.println("List is empty");
+            returnItem = null;
+        } else if (currentPos == null) {
+            currentPos = head;
+            returnItem = currentPos.info;
+        } else {
+            returnItem = currentPos.info;
+            currentPos = currentPos.next;
+        }
+        return returnItem;
+    }
+
+    public void resetList() {
+        currentPos = null;
+    }
+
+    public SortedLinkedList mergeList(SortedLinkedList list) {
+        SortedLinkedList returnList = new SortedLinkedList();
+        for(int count = 0; count < this.getLength(); count++) {
+            returnList.insertItem(this.getNextItem());
+        }
+        for(int count = 0; count < list.getLength(); count++) {
+            returnList.insertItem(list.getNextItem());
+        }
+        return returnList;   
+    }
+
+    public void deleteAlternateNodes() {
+        NodeType temp = head;
+        for(int count = 0; count < this.getLength(); count++) {
+            if (count % 2 == 0) {
+                deleteItem(temp.info);
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void intersection(SortedLinkedList list) {
+        SortedLinkedList printList = new SortedLinkedList();
+        NodeType temp = this.head;
+        while (temp != null) {
+            if(list.searchItem(temp.info) != -1) {
+                printList.insertItem(temp.info);
+            }
+            temp = temp.next;
+        }
+        System.out.print("Intersection:");
+        temp = printList.head;
+        while (temp != null) {
+            System.out.println(" " + temp.info);
+            temp = temp.next;
+        }
+    }
+}
