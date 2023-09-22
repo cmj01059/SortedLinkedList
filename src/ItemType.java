b0bff48d54c1c43a7cf87111434cc65d3d75181f
Canon Jackson <cmj01059@uga.edu>
2023-09-21 21:46:43 -0400

    ItemType intital implementation

---
+++ src/ItemType.java
@@ -0,0 +1,21 @@
+public class ItemType {
+    
+    private int value;
+
+    public ItemType(int value) {
+        this.value = value;
+    }
+
+    public int compareTo(ItemType item) {
+        if (item.getValue() > value)
+            return -1;
+        else if (item.getValue() < value)
+            return 1;
+        else
+            return 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
